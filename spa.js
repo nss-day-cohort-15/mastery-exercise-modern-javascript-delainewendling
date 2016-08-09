9353bd9874640da90935e9157c6dfb7abf50c536
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-09 12:45:53 -0500

    Wrote code to switch between views to eventually get to the battlefield

--- spa.js
+++ spa.js
@@ -2,13 +2,30 @@ var BattleDome = (function(player){
   //Grab variables from the DOM
   $playerContainer = $(".players")
   // $player1View = $playerContainer.find("#player1View")
-  // $player2View = $playerContainer.find("#player2View")
+  $player2View = $playerContainer.find("#player2View")
+  $battleView = $playerContainer.find("#battleView")
   $player1Name = $playerContainer.find("#player1Name")
   $player2Name = $playerContainer.find("#player2Name")
   $robotImage = $playerContainer.find(".robotImg")
+  $submitPlayer1 = $playerContainer.find("#player1submit")
+  $submitPlayer2 = $playerContainer.find("#player2submit")
 
   //bind events
   $robotImage.on("click", robotClicked)
+  $submitPlayer1.on("click", goToPlayer2)
+  $player1Name.on("keypress", (e)=>{
+    e.which = e.which || e.keyCode;
+    if(e.which == 13) {
+      goToPlayer2()
+    }
+  })
+  $submitPlayer2.on("click", goToBattle)
+  $player2Name.on("keypress", (e)=>{
+    e.which = e.which || e.keyCode;
+    if(e.which == 13) {
+      goToBattle()
+    }
+  })
 
   function robotClicked(e) {
     console.log("robot clicked", e)
@@ -21,11 +38,17 @@ var BattleDome = (function(player){
 
   function goToPlayerName(playerNumber) {
     $(`#player${playerNumber}Name`).removeClass("hidden").addClass("selected")
-    goToPlayer2()
   }
 
-  function goToPlayer2 (){
-    
+  function goToPlayer2() {
+    $player1Name.removeClass("selected").addClass("hidden")
+    $player2View.removeClass("hidden").addClass("selected")
   }
 
+  function goToBattle(){
+    $player2Name.removeClass("selected").addClass("hidden")
+    $battleView.removeClass("hidden").addClass("selected")
+  }
+
+  return player
 })(BattleDome || {})
